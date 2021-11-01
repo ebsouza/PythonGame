ab6049f3631f4e979a1aaeedfe85886ae74ea1b6
ebsouza <ebsouza07@yahoo.com>
2021-11-01 11:42:57 -0300

    add statistics

--- modules/question.py
+++ modules/question.py
@@ -4,6 +4,8 @@ import random
 import os
 import time
 
+from modules.statistics import Statistics
+
 
 class SumGenerator:
 
@@ -83,8 +85,9 @@ class Manager:
 
     def __init__(self, generator_code, prev_screen):
         self.generator = self.GENERATOR_MAPPER[generator_code]
-        self.result = {}
+        self.result = dict()
         self.prev_screen = prev_screen
+        self.statistics = Statistics()
 
     def requestInput(self):
         c = input("")
@@ -93,20 +96,30 @@ class Manager:
             self.prev_screen["reference"].print()
         else:
             self.execute()
-            print("Parabéns você completou a partida.")
-            print(self.result)
+            self.statistics.saveRecords(self.result["correct"],
+                                        self.result["incorrect"],
+                                        self.result["duration"])
             time.sleep(5)
             self.prev_screen["reference"].print()
 
     def execute(self):
         questions = self.generator.generate_question(2)
-        self.result = {"correct": 0, "incorrect": 0}
+        self.init_result()
 
+        start_time = time.time()
         for question in questions:
             question.print()
             self.check_answer(question)
             time.sleep(1)
+
+        self.result["duration"] = time.time() - start_time
         os.system("clear")
+        self.print_result()
+
+    def init_result(self):
+        self.result = {"correct": 0,
+                       "incorrect": 0,
+                       "duration": 0}
 
     def check_answer(self, question):
         print("")
@@ -114,10 +127,16 @@ class Manager:
 
         if question.alternatives["ans"] == ans:
             self.result["correct"] += 1
-            print("Respota correta :)")
+            print("\nRespota CORRETA")
         else:
             self.result["incorrect"] += 1
-            print("Resposta INcorreta :/")
+            print("\nResposta incorreta")
+
+    def print_result(self):
+        print("Parabéns você completou a partida. \n")
+        print("Acertos: {}".format(self.result["correct"]))
+        print("Erros: {}".format(self.result["incorrect"]))
+        print("Duração: {:.1f} segundos".format(self.result["duration"]))
 
     def print(self):
         os.system("clear")
---
+++ modules/statistics.py
@@ -0,0 +1,21 @@
+# coding=UTF-8
+
+import os
+
+class Statistics:
+
+    def __init__(self):
+        self.file = open("statistics.csv", "a")
+
+        if os.path.getsize("statistics.csv") == 0:
+            self.create_header()
+
+        self.file.close()
+
+    def create_header(self):
+        self.file.write('Correct, Incorrect, Duration')
+
+    def saveRecords(self, correct, incorrect, duration):
+        self.file = open("statistics.csv", "a")
+        self.file.write('\n' + str(correct) + ', ' + str(incorrect) + ',  ' + str(duration))
+        self.file.close()
