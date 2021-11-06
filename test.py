ee72eb756228c7789a6d0bca76d72ea3ec175de0
ebsouza <ebsouza07@yahoo.com>
2021-11-06 10:40:55 -0300

    add GeneratorTest cases

--- test.py
+++ test.py
@@ -2,37 +2,106 @@
 
 import unittest
 from modules.question import SumGenerator, SubGenerator, MultGenerator
+
 from modules.utils import unique_non_zero_random_numbers
+
+
+class AbstractGeneratorTest(unittest.TestCase):
+    def setUp(self):
+        self.generator = SumGenerator()
+        self.a = 7
+        self.b = 14
+
+    def test_generate_question_default_length(self):
+        questions = self.generator.generate_question()
+        self.assertEqual(len(questions), 10)
+
+    def test_generate_question_defined_length(self):
+        questions = self.generator.generate_question(5)
+        self.assertEqual(len(questions), 5)
+
+    def test_generate_alternatives_keys(self):
+        alternatives = self.generator.generate_alternatives(self.a, self.b)
+        alternatives_keys = alternatives.keys()
+
+        self.assertIn("ans", list(alternatives_keys))
+        self.assertIn("a", list(alternatives_keys))
+        self.assertIn("b", list(alternatives_keys))
+        self.assertIn("c", list(alternatives_keys))
+        self.assertIn("d", list(alternatives_keys))
+
+    def test_generate_alternatives_values(self):
+        alternatives = self.generator.generate_alternatives(self.a, self.b)
+        alternatives_values_set = set(alternatives.values())
+
+        alternatives_keys = alternatives.keys()
+
+        self.assertEqual(len(alternatives_values_set), len(alternatives_keys) - 1)
 
 
 class SumGeneratorTest(unittest.TestCase):
 
     def setUp(self):
         self.generator = SumGenerator()
+        self.a = 5
+        self.b = 5
 
-    def test_question_length(self):
-        questions = self.generator.generate_question()
-        self.assertEqual(len(questions), 10)
+    def test_text(self):
+        text = self.generator.text(self.a, self.b)
+        self.assertEqual(text, f"{self.a} + {self.b}")
+
+    def test_operate(self):
+        result = self.generator.operate(self.a, self.b)
+        self.assertEqual(result, self.a + self.b)
+
+    def test_generate_a_b(self):
+        a, b = self.generator.generate_a_b()
+        self.assertTrue(10 <= a <= 20)
+        self.assertTrue(10 <= b <= 20)
 
 
 class SubGeneratorTest(unittest.TestCase):
 
     def setUp(self):
         self.generator = SubGenerator()
+        self.a = 5
+        self.b = 5
 
-    def test_question_length(self):
-        questions = self.generator.generate_question()
-        self.assertEqual(len(questions), 10)
+    def test_text(self):
+        text = self.generator.text(self.a, self.b)
+        self.assertEqual(text, f"{self.a} - {self.b}")
+
+    def test_operate(self):
+        result = self.generator.operate(self.a, self.b)
+        self.assertEqual(result, self.a - self.b)
+
+    def test_generate_a_b(self):
+        a, b = self.generator.generate_a_b()
+        self.assertTrue(a >= b)
+        self.assertTrue(15 <= a <= 30)
+        self.assertTrue(10 <= b <= 20)
 
 
 class MultGeneratorTest(unittest.TestCase):
 
     def setUp(self):
         self.generator = MultGenerator()
+        self.a = 5
+        self.b = 5
 
-    def test_question_length(self):
-        questions = self.generator.generate_question()
-        self.assertEqual(len(questions), 10)
+    def test_text(self):
+        text = self.generator.text(self.a, self.b)
+        self.assertEqual(text, f"{self.a} * {self.b}")
+
+    def test_operate(self):
+        result = self.generator.operate(self.a, self.b)
+        self.assertEqual(result, self.a * self.b)
+
+    def test_generate_a_b(self):
+        a, b = self.generator.generate_a_b()
+        self.assertTrue(a <= b)
+        self.assertTrue(5 <= a <= 10)
+        self.assertTrue(5 <= b <= 10)
 
 
 class UtilsTest(unittest.TestCase):
